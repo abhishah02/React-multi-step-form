1b745e192d1e4552a37c9e2960553a9e6489196f
abhishah02 <abhishah578@gmail.com>
2022-02-23 18:00:10 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import LinearStepper from "./LinearStepper";
+import { CssBaseline, Container, Paper, Box } from "@material-ui/core";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CssBaseline />
+      <Container component={Box} p={4}>
+        <Paper component={Box} p={3}>
+          <LinearStepper />
+        </Paper>
+      </Container>
+    </>
   );
-}
+};
 
 export default App;
